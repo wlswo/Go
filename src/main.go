3de9619c72e7ab115512bc2e00c5e4d563188dc8
JaeJinByun <byunjin11@naver.com>
2022-06-15 09:21:10 +0900

    Main Change

--- src/main.go
+++ src/main.go
@@ -7,13 +7,16 @@ import (
 
 func main() {
 	bc := BLC.NewBlockchain()
-	bc.AddBlock("1번째 트랜잭션")
-	bc.AddBlock("2번째 트랜잭션")
+	for i := 0; i < 11; i++ {
+		bc.AddBlock("트랜잭션")
+	}
+
 	for i, bc := range bc.Blocks {
-		f.Println("-------------------------------", i, "번째 블록 데이터 --------------------------------")
+		f.Println("-------------------------------", i, "번째 블록 데이터 -------------------------------")
 		f.Printf("이전블록 해시    : %x\n", bc.PrevBlockHash)
 		f.Printf("데이터           : %s\n", bc.Data)
 		f.Printf("현재 블럭의 hash : %x\n", bc.Hash)
+		f.Printf("만들어진 시각    : %d\n", bc.Timestamp)
 		f.Println("-----------------------------------------------------------------------------------\n")
 	}
 }
