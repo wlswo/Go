6e55e2946bcb3bde0233bf8961088ddecafd4027
JaeJinByun <byunjin11@naver.com>
2022-06-26 16:19:22 +0900

    main Change

--- src/main.go
+++ src/main.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"Project"
-	"fmt"
 )
 
 func main() {
-	Project.Run()
-
-	wallet := Project.NewWallet()
-	address := wallet.GetAddress()
-
-	fmt.Println(Project.ValidateAddress(address))
-	fmt.Println(address)
-	/* 트랜잭션들 관리 */
-	/* 트랜잭션에 대한 서명 */
+	Project.StartServer()
 
 }
